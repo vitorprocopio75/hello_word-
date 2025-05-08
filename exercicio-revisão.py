aeb7fdbc8086236f8135e82bc194cc3d89df1b92
vitorprocopio75 <vitor.nascimento.pontes@gmail.com>
2025-05-08 20:04:21 +0000

    criação de operação divisão

---
+++ exercicio-revisão.py
@@ -0,0 +1,13 @@
+numero1 = int(input("Insira o primeiro número da divisão :"))
+numero2 = int(input("Insira o segundo número da divisão :"))
+        #divisão com resultado float = flutuante 
+divisisao_float = numero1 / numero2
+
+print(divisisao_float)
+        #divisão com resultado inteiro
+divisisao_int = numero1 //  numero2
+print(divisisao_int)
+#divisão com resto 
+divisao_resto = numero1 % numero2
+print(divisao_resto)
+#
