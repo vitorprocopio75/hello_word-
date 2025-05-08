0e02f543175df438de0e035a6617007657d22eda
vitorprocopio75 <vitor.nascimento.pontes@gmail.com>
2025-05-08 20:38:21 +0000

    crie exercicio media

---
+++ media.py
@@ -0,0 +1,20 @@
+            #você foi contratado para criar um programa de calculo de média de notas escolares 
+
+            #esse programa recebera duas notas e calculará a media
+nota1 = int(input("insira a sua primeira nota:"))
+nota2 = int(input("Insira segunda nota :"))
+
+media = nota1 + nota2 / 2 
+print(media)
+
+if media >= 5:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+            #1solicitar o input das notas numéricas [int ou float]
+            #2 realizar o calculo de media = (nota1 + nota2)/2 
+            #3 verificar se o aluno foi aprovado ou reprovado ou aprovado, para isso vamos fazer uma comparação
+
+#dia 8/05/2025
+
