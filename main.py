14bbc41f0d5b7847826b85513f079fdd728cd4fc
vitorprocopio75 <vitor.nascimento.pontes@gmail.com>
2025-05-12 17:20:51 -0300

    operadores booleanos main.py 12/05
    Operadores booleanos 
    aula do dia 12/05

---
+++ main.py
@@ -0,0 +1,27 @@
+        #operadores booleanos verdadeiro(true) ou falso (false)
+
+a = 3 > 4
+        #print(a)
+        # sinal de diferente  (!=)
+b = 7 != 7 
+print(b)
+        #comparação composta
+composto = 9 == 10 - 1
+#PRINT(Composto)
+
+composto2 = 5 + 4 <= 11 - 2
+print(composto2)
+        # * = vezes, ** = potencia,  / = divisão, // = float
+divisao_saida_float = 4 / 2
+print(f'{divisao_saida_float}')
+
+divisao_saida_int = 4 // 2 
+print(f"{divisao_saida_int}")
+
+divisao_resto = 4 % 2 
+print(f"{divisao_resto}")
+
+resultado = 3 + 4 * 2 
+print(f'Esse é o resultado do cáculo de 3 + 4 * 2 é: {resultado}')
+
+
