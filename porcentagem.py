b536c8a0ed2a9f55b0d031e6346531ae70f002a5
vitorprocopio75 <vitor.nascimento.pontes@gmail.com>
2025-05-12 17:52:05 -0300

    media 12/5
    media 
    12/5

---
+++ porcentagem.py
@@ -0,0 +1,16 @@
+        #pede pro usuario digitar uma nota 
+nota1 = float(input("Digite uma nota: "))
+
+
+
+        # pede pro usuario digitar a segunda nota 
+nota2 = float(input("A segunda nota: "))
+
+
+        # calculo da media 
+media = (nota1 + nota2) / 2
+
+
+        # exibindo nota 
+print(f"O resultado da sua média é : {media}")
+
